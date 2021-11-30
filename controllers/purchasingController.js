d10a7d78b5908721e8197ff10bfb95cc1beaee8a
puimekster <hoanhao18102000@gmail.com>
2021-11-30 13:47:05 +0700

    fix

--- controllers/purchasingController.js
+++ controllers/purchasingController.js
@@ -21,9 +21,9 @@ let user = {};
 
 exports.purchase = catchAsync(async (req, res, next) => {
 	// 1) Lấy thông tin user và thông tin giỏ hàng
-	user = await User.findById(req.user.id);
+	// user = await User.findById(req.user.id);
+	user = await User.findById(req.params.userId);
 	console.log(user);
-	// user = await User.findById(req.params.userId);
 	const cart = user.cart;
 	const cartItems = cart.items;
 
